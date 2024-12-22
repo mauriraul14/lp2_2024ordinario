8fd1ef4ad753c0730b6fafc5630d291fed1f69a2
mauriraul14 <mauriraulqui12@gmail.com>
2024-12-22 15:54:08 -0300

    2da parte del examen

---
+++ src/main/java/io/quarkus/Gasto.java
@@ -0,0 +1,43 @@
+package io.quarkus;
+
+public class Gasto {
+    private int id;
+    private String descripcion;
+    private long fecha;
+    private double monto;
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public long getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(long fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    
+}
